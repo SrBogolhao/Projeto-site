f709b6ab9a77b94622506b6f197be9e49d6daf3b
Davi Roberto Santos da Silva <drss.davi@gmail.com>
2022-05-03 19:41:13 -0300

    f

--- JavaScript K/script.js
+++ JavaScript K/script.js
@@ -1,1 +1,2 @@
-
+window.document.querySelector(`#player`);
+inner.Html=<iframe width="560" height="315" src="https://www.youtube.com/embed/zoFBVnmwqUw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
