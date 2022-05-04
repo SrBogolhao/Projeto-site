6b531bfecc2881f00eb11f5418ad8eaafccd33de
Davi Roberto Santos da Silva <drss.davi@gmail.com>
2022-05-04 13:00:43 -0300

    e

--- Kultivi/JavaScript K/script.js
+++ Kultivi/JavaScript K/script.js
@@ -1,3 +1,4 @@
-window.document.querySelector(`#player`);
-window.document.getElementById(`#player`)
-inner.Html=<iframe width="560" height="315" src="https://www.youtube.com/embed/zoFBVnmwqUw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+function clicar(){
+    var play=window.document.getElementById(`player`)
+    play.innerHTML=`<iframe width="560" height="315" src="https://www.youtube.com/embed/zoFBVnmwqUw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
