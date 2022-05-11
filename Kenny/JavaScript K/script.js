2885c715b7f657a351b4ba7d7a51ad868fc243d1
Davi Roberto Santos da Silva <drss.davi@gmail.com>
2022-05-11 19:12:56 -0300

    2

---
+++ Kenny/JavaScript K/script.js
@@ -0,0 +1,40 @@
+function aula1(){
+    var vid = window.document.getElementById(`player`)
+    vid.innerHTML =`<iframe width="560" height="315" src="https://www.youtube.com/embed/g6eC2MQ1Kv8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function aula2(){
+    var vid = window.document.getElementById(`player`)
+    vid.innerHTML =`<iframe width="560" height="315" src="https://www.youtube.com/embed/prAINVhqreQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function aula3(){
+    var vid = window.document.getElementById(`player`)
+    vid.innerHTML =`<iframe width="560" height="315" src="https://www.youtube.com/embed/Fx1H0rMv7Tc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function aula4(){
+    var vid = window.document.getElementById(`player`)
+    vid.innerHTML =`<iframe width="560" height="315" src="https://www.youtube.com/embed/vqeuKn66MLw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function aula5(){
+    var vid = window.document.getElementById(`player`)
+    vid.innerHTML =`<iframe width="560" height="315" src="https://www.youtube.com/embed/DgYgwqkGWbM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function aula6(){
+    var vid = window.document.getElementById(`player`)
+    vid.innerHTML =`<iframe width="560" height="315" src="https://www.youtube.com/embed/Yfysh7_TSLQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function aula7(){
+    var vid = window.document.getElementById(`player`)
+    vid.innerHTML =`<iframe width="560" height="315" src="https://www.youtube.com/embed/Yfysh7_TSLQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function aula8(){
+    var vid = window.document.getElementById(`player`)
+    vid.innerHTML =`<iframe width="560" height="315" src="https://www.youtube.com/embed/Jepam2PotJA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function aula9(){
+    var vid = window.document.getElementById(`player`)
+    vid.innerHTML =`<iframe width="560" height="315" src="https://www.youtube.com/embed/Jepam2PotJA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function aula10(){
+    var vid = window.document.getElementById(`player`)
+    vid.innerHTML =`<iframe width="560" height="315" src="https://www.youtube.com/embed/vn6_8Qi6mrY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
