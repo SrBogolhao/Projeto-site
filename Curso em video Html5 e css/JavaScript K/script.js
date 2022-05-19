a19d96dc98bd7e8a67da1126b3e36f072c57f6e6
Davi Roberto Santos da Silva <drss.davi@gmail.com>
2022-05-19 18:35:19 -0300

    sei la

---
+++ Curso em video Html5 e css/JavaScript K/script.js
@@ -0,0 +1,40 @@
+function aula1(){
+    var vid = window.document.getElementById(`player`)
+    vid.innerHTML =`<iframe width="560" height="315" src="https://www.youtube.com/embed/Ejkb_YpuHWs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function aula2(){
+    var vid = window.document.getElementById(`player`)
+    vid.innerHTML =`<iframe width="560" height="315" src="https://www.youtube.com/embed/Ejkb_YpuHWs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function aula3(){
+    var vid = window.document.getElementById(`player`)
+    vid.innerHTML =`<iframe width="560" height="315" src="https://www.youtube.com/embed/jgQjeqGRdgA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function aula4(){
+    var vid = window.document.getElementById(`player`)
+    vid.innerHTML =`<iframe width="560" height="315" src="https://www.youtube.com/embed/57wyfS560Uk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function aula5(){
+    var vid = window.document.getElementById(`player`)
+    vid.innerHTML =`<iframe width="560" height="315" src="https://www.youtube.com/embed/0zLjVhHdOm8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function aula6(){
+    var vid = window.document.getElementById(`player`)
+    vid.innerHTML =`<iframe width="560" height="315" src="https://www.youtube.com/embed/F74GKCLXUWM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function aula7(){
+    var vid = window.document.getElementById(`player`)
+    vid.innerHTML =`<iframe width="560" height="315" src="https://www.youtube.com/embed/F74GKCLXUWM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function aula8(){
+    var vid = window.document.getElementById(`player`)
+    vid.innerHTML =`<iframe width="560" height="315" src="https://www.youtube.com/embed/RFHSt1PCy0k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function aula9(){
+    var vid = window.document.getElementById(`player`)
+    vid.innerHTML =`<iframe width="560" height="315" src="https://www.youtube.com/embed/B4FU3NFRTDw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function aula10(){
+    var vid = window.document.getElementById(`player`)
+    vid.innerHTML =`<iframe width="560" height="315" src="https://www.youtube.com/embed/iSqf2iPqJNM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
---
+++ Curso em video Js/JavaScript K/script.js
@@ -0,0 +1,40 @@
+function aula1(){
+    var vid = window.document.getElementById(`player`)
+    vid.innerHTML =`<iframe width="560" height="315" src="https://www.youtube.com/embed/BXqUH86F-kA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function aula2(){
+    var vid = window.document.getElementById(`player`)
+    vid.innerHTML =`<iframe width="560" height="315" src="https://www.youtube.com/embed/uzEhd3Lugik" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function aula3(){
+    var vid = window.document.getElementById(`player`)
+    vid.innerHTML =`<iframe width="560" height="315" src="https://www.youtube.com/embed/Ptbk2af68e8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function aula4(){
+    var vid = window.document.getElementById(`player`)
+    vid.innerHTML =`<iframe width="560" height="315" src="https://www.youtube.com/embed/Ptbk2af68e8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function aula5(){
+    var vid = window.document.getElementById(`player`)
+    vid.innerHTML =`<iframe width="560" height="315" src="https://www.youtube.com/embed/FjT97HVT5g8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function aula6(){
+    var vid = window.document.getElementById(`player`)
+    vid.innerHTML =`<iframe width="560" height="315" src="https://www.youtube.com/embed/FdePtO5JSd0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function aula7(){
+    var vid = window.document.getElementById(`player`)
+    vid.innerHTML =`<iframe width="560" height="315" src="https://www.youtube.com/embed/OmmJBfcMJA8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function aula8(){
+    var vid = window.document.getElementById(`player`)
+    vid.innerHTML =`<iframe width="560" height="315" src="https://www.youtube.com/embed/Vbabsye7mWo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function aula9(){
+    var vid = window.document.getElementById(`player`)
+    vid.innerHTML =`<iframe width="560" height="315" src="https://www.youtube.com/embed/OJgu_KCCUSY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function aula10(){
+    var vid = window.document.getElementById(`player`)
+    vid.innerHTML =`<iframe width="560" height="315" src="https://www.youtube.com/embed/hZG9ODUdxHo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
