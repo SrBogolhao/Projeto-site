8a141f9e8c3c17ba52872dbb56d2dd659d6e8cd5
Davi Roberto Santos da Silva <drss.davi@gmail.com>
2022-06-04 18:48:54 -0300

    w

--- Instagram/script.js
+++ Instagram/script.js
@@ -1,10 +1,10 @@
 var video = document.querySelector(`video`);
 
 navigator.mediaDevices.getUserMedia({video:true})
-.then(stream =>{
+.then(stream => {
     video.srcObject = stream;
     video.play();
 })
-.catch(error=>{
+.catch(error => {
     console.log(error);
 })
