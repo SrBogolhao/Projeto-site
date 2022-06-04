4386d85b6e8baf1bfcdc28e486e54b4811a35d00
Davi Roberto Santos da Silva <drss.davi@gmail.com>
2022-06-04 17:24:30 -0300

    d

--- Instagram/script.js
+++ Instagram/script.js
@@ -8,13 +8,3 @@ navigator.mediaDevices.getUserMedia({video:true})
 .catch(error=>{
     console.log(error);
 })
-
-    var canvas = document.querySelector(`canvas`);
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    var context = canvas.getContext(`2d`);
-    context.drawImage(`video`, 0, 0);
-    link.window = `photo.png`;
-    link. href = canvas.toDataURL();
-    link.textContent = `clique para baixar a imagem`;
-    document.body.appendChild(link);
